090d4676cd6b232ee5b2a639441e49ab7164d621
Abhay Nepal <abhay.nepal.9@gmail.com>
2023-05-14 20:31:57 +0545

    'final commit'

--- MyAppWeb/Controllers/CategoryController.cs
+++ MyAppWeb/Controllers/CategoryController.cs
@@ -20,6 +20,62 @@ namespace MyAppWeb.Controllers
         }
 
         [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            return View(category);
+        }
+        //edit post
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                TempData["success"] = "Data Updated !!";
+                return RedirectToAction("Index");
+            }
+            return (View(category));
+        }
+
+        //delet get
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            return View(category);
+        }
+        //delete post
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                TempData["success"] = "Deleted !!";
+                return RedirectToAction("Index");
+            }
+            return (View(category));
+        }
+
+
+
+        [HttpGet]
         public IActionResult Create()
         {
 
@@ -33,6 +89,7 @@ namespace MyAppWeb.Controllers
             if(ModelState.IsValid){
                 _context.Categories.Add(category);
                 _context.SaveChanges();
+                TempData["success"] = "Data Added !!";
                 return RedirectToAction("Index");
             }
             return (View(category));
